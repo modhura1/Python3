06a5b9c16f1f80a8cd8d1e5f37c90b552036c26a
Modhura <32320483+modhura3797@users.noreply.github.com>
2019-05-28 00:29:11 +0530

    Delete Migratory_Birds.py

--- Hackerrank/Algorithms/Implementation/Migratory_Birds.py
+++
@@ -1,6 +0,0 @@
-#Problem : https://www.hackerrank.com/challenges/migratory-birds/problem
-
-#!/bin/python3
-arr_count = int(input().strip())
-arr = list(map(int, input().rstrip().split()))
-print(max(set(arr), key = arr.count))
