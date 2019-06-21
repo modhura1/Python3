760c475f33ab13e5bb86a9830e6b5ffd8c6f2166
Modhura <32320483+modhura3797@users.noreply.github.com>
2019-06-22 02:00:40 +0530

    Update Sum_vs_XOR.py

--- HackerRank/Algorithms/Bit_Manipulation/Sum_vs_XOR.py
+++ HackerRank/Algorithms/Bit_Manipulation/Sum_vs_XOR.py
@@ -1,15 +1,16 @@
 #Problem : https://www.hackerrank.com/challenges/sum-vs-xor/problem
 
-count = 0
 n = int(input())
-if ( n == 0):
-    count = 1
-else:
-    for i in range(n):
-        if n + i == n ^ i:
-            count += 1
-        else:
-            continue
-print(count)
+print(1 << (n.bit_length() - bin(n).count('1')))
 
-#80% solved
+'''
+Totally a mathematical trick.
+just count total numbers of ZERO present in binary number of given n, 
+and answer will be the 2 to the power of (total num of zero).
+
+It is simply concept of the Hamming weight of a number. 
+In a lower-level language, One probably uses a nifty SIMD within a register trick or a library function to compute this. 
+In Python, the shortest and most efficient way is to just turn it into a binary string and count the '1's: bin(n).count('1')
+
+You can get the number of zeros by subtracting ones(num) from the total number of digits : n.bit_length() - bin(n).count('1'))
+'''
