d933c62bb539b1be5af21e0ada3d9d33bbd0bf59
Modhura <32320483+modhura3797@users.noreply.github.com>
2019-06-23 22:11:31 +0530

    Update Mini-Max_Sum.py

--- HackerRank/Algorithms/Warmup/Mini-Max_Sum.py
+++ HackerRank/Algorithms/Warmup/Mini-Max_Sum.py
@@ -1,5 +1,8 @@
 #Problem : https://www.hackerrank.com/challenges/mini-max-sum/problem
 
+
+### APROACH 1 :
+
 #!/bin/python3
 
 def miniMaxSum(arr):
@@ -14,3 +17,18 @@ if __name__ == '__main__':
     
     
 #Complexity : O(NlogN)
+
+
+### APROACH 2 :
+
+def miniMaxSum(arr):
+    Min = min(arr)
+    Max = max(arr)
+    maxSum = sum (arr, -Min) 
+    minSum = sum (arr, -Max) 
+    print (str(minSum)+" "+str(maxSum))
+
+arr = list(map(int, input().rstrip().split()))
+miniMaxSum(arr)
+
+#Complexity : O(N)
