da41febdc79244f534ae0a276064cf278a4ed0c2
Modhura <32320483+modhura3797@users.noreply.github.com>
2019-06-23 20:09:45 +0530

    Update Xor_and_Sum.py

--- HackerRank/Algorithms/Dynamic_Programming/Xor_and_Sum.py
+++ HackerRank/Algorithms/Dynamic_Programming/Xor_and_Sum.py
@@ -1,13 +1,9 @@
-#Problem : 
+#Problem : https://www.hackerrank.com/challenges/xor-and-sum/problem
 
 a = int(input(), 2)
 b = int(input(), 2)
 
 res = 0
 for i in range(314160):
-    #b = b << i 
-    #a = a ^ b
     res += a ^ (b << i)
 print(res % 1000000007)
-
-#60% solved
