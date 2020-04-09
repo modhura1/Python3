7c0a25f504f102a30ade6721fc449a0a85f6517c
Modhura <32320483+modhura3797@users.noreply.github.com>
2020-04-10 01:49:56 +0530

    Create Staircase.py

---
+++ HackerRank/Algorithms/Warmup/Staircase.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/staircase/problem
+
+n = int(input())
+for i in range(1,n+1):
+    for j in range (n-i):
+        print(' ',end='')
+    for k in range (i):
+        print('#',end='')
+    print('')
