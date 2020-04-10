78450410a16ccfcdb4883f1ba8540368c3df8d27
Modhura <32320483+modhura3797@users.noreply.github.com>
2020-04-10 23:27:04 +0530

    Create RecursiveDigitSumBrilliantSol.py

---
+++ HackerRank/Algorithms/Recursion/RecursiveDigitSumBrilliantSol.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/recursive-digit-sum/problem
+
+
+n, k = map(int, input().split())
+x = n * k % 9
+print(x if x else 9)
+
+
+# Logic : http://applet-magic.com/digitsummod9.htm
