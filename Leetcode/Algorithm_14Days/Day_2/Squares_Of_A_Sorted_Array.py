2fcd2757b325ba8da13f7888c060fde3bf1fd29b
Modhura <32320483+modhura3797@users.noreply.github.com>
2022-04-07 14:52:42 +0530

    Update Squares_Of_A_Sorted_Array.py

--- Leetcode/Algorithm_14Days/Day_2/Squares_Of_A_Sorted_Array.py
+++ Leetcode/Algorithm_14Days/Day_2/Squares_Of_A_Sorted_Array.py
@@ -11,3 +11,12 @@ class Solution:
 """ Efficiency:
   Runtime: 224 ms, faster than 92.39% of Python3 online submissions for Squares of a Sorted Array.
   Memory Usage: 15.8 MB, less than 90.23% of Python3 online submissions for Squares of a Sorted Array. """
+
+
+
+
+#------------------------------------Another approach----------------------------------
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        nums=[n*n for n in nums]
+        return sorted(nums)
