856fb4bee434483be9a64637e5da880e3ca18141
Modhura <32320483+modhura3797@users.noreply.github.com>
2022-04-08 22:22:18 +0530

    Update Two-Sum-ii-Input-Array-Is-Sorted.py

--- Leetcode/Algorithm_14Days/Day_3/Two-Sum-ii-Input-Array-Is-Sorted.py
+++ Leetcode/Algorithm_14Days/Day_3/Two-Sum-ii-Input-Array-Is-Sorted.py
@@ -27,22 +27,20 @@ class Solution:
 #--------------------------------------Another Approach-------------------------
 
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        if sum(nums)!=0:
-            l=len(nums)
-            i=0
-            while i<l:
-                if nums[i]==0:
-                    nums.pop(i)
-                    nums.append(0)
-                    i=0
-                    l-=1
-                else:
-                    i+=1
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        a=b=c=0
+        for i in numbers:
+            t=target-i
+            sub=numbers[c+1:]
+            if t in sub:
+                a=c
+                b=sub.index(t)+c+1
+                break
+            c+=1
+        a+=1
+        b+=1
+        return [a,b]
       
 """ Efficiency:
-  Runtime: 3628 ms, faster than 5.00% of Python3 online submissions for Move Zeroes.
-Memory Usage: 15.5 MB, less than 70.50% of Python3 online submissions for Move Zeroes."""
+  Runtime: 9696 ms, faster than 5.03% of Python3 online submissions for Two Sum II - Input Array Is Sorted.
+Memory Usage: 14.9 MB, less than 91.20% of Python3 online submissions for Two Sum II - Input Array Is Sorted."""
