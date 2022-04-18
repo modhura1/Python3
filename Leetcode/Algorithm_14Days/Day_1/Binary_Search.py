0becf5f942880e3fa488810b71b9ee7dd8bd656e
Modhura <32320483+modhura3797@users.noreply.github.com>
2022-04-19 03:04:45 +0530

    Create Binary_Search.py

---
+++ Leetcode/Algorithm_14Days/Day_1/Binary_Search.py
@@ -0,0 +1,24 @@
+#problem: https://leetcode.com/problems/binary-search/
+#problem code: 704
+#Difficulty: Easy
+
+class Solution:    
+    def search(self, nums: List[int], target: int) -> int:
+        l=0
+        r=len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                l=mid+1
+            elif nums[mid]>target:
+                r=mid-1
+        return -1    
+    
+    
+"""
+Efficiency: 
+    Runtime: 541 ms, faster than 5.00% of Python3 online submissions for Binary Search.
+    Memory Usage: 15.4 MB, less than 97.81% of Python3 online submissions for Binary Search.
+"""
