26230821593a710758ebb732126487869f96150d
Modhura <32320483+modhura3797@users.noreply.github.com>
2022-04-18 22:34:18 +0530

    Update Number_Of_1_Bits.py

--- Leetcode/Algorithm_14Days/Day_13/Number_Of_1_Bits.py
+++ Leetcode/Algorithm_14Days/Day_13/Number_Of_1_Bits.py
@@ -19,3 +19,19 @@ Efficiency:
   Runtime: 25 ms, faster than 96.83% of Python3 online submissions for Number of 1 Bits.
   Memory Usage: 13.7 MB, less than 95.45% of Python3 online submissions for Number of 1 Bits.
 """
+
+
+#------------------------------------More Efficient Approach----------------------------------------------------------
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        c=0
+        for i in bin(n)[2:]:
+            if i=='1':
+                c+=1
+        return c
+    
+"""
+Efficiency:
+    Runtime: 23 ms, faster than 99.06% of Python3 online submissions for Number of 1 Bits.
+    Memory Usage: 13.8 MB, less than 95.45% of Python3 online submissions for Number of 1 Bits.
+"""
