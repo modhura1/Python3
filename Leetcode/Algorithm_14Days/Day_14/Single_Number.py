e7183e82983c5e48105002791f306b58c0c2e2fb
Modhura <32320483+modhura3797@users.noreply.github.com>
2022-04-19 11:52:32 +0530

    Update Single_Number.py

--- Leetcode/Algorithm_14Days/Day_14/Single_Number.py
+++ Leetcode/Algorithm_14Days/Day_14/Single_Number.py
@@ -37,3 +37,20 @@ Efficiency:
     Runtime: 198 ms, faster than 45.12% of Python3 online submissions for Single Number.
     Memory Usage: 16.1 MB, less than 99.09% of Python3 online submissions for Single Number.
 """
+
+
+
+#---------------------------------Approach 3---------------------------------------
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        i=0
+        for j in nums:
+            i=i^j
+        return i
+
+"""
+Efficiency:
+    Runtime: 152 ms, faster than 72.63% of Python3 online submissions for Single Number.
+    Memory Usage: 16.7 MB, less than 53.84% of Python3 online submissions for Single Number.
+"""
