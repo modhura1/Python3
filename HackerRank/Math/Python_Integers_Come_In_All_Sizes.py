6fac1825196907b668869a8d54c3fcc7eea9e6af
Modhura <32320483+modhura3797@users.noreply.github.com>
2022-04-20 01:31:02 +0530

    Create Python_Integers_Come_In_All_Sizes.py

---
+++ HackerRank/Math/Python_Integers_Come_In_All_Sizes.py
@@ -0,0 +1,4 @@
+#https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true
+#Difficulty : Easy
+
+print((int(input())**int(input()))+(int(input())**int(input())))
