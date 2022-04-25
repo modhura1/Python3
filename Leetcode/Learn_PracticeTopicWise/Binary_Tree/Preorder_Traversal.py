a626ad4758a82f0e86f1bb21b3a30f0fdc00a0ff
Modhura <32320483+modhura3797@users.noreply.github.com>
2022-04-25 22:40:11 +0530

    Update Preorder_Traversal.py

--- Leetcode/Learn_PracticeTopicWise/Binary_Tree/Preorder_Traversal.py
+++ Leetcode/Learn_PracticeTopicWise/Binary_Tree/Preorder_Traversal.py
@@ -4,7 +4,7 @@
 #problem code : 144
 
 
-
+#------------------------------------Recursive-----------------------------------------------
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
